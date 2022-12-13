256c3c87d8ab19b325df0379704bede6c61211c1
leo <leoextreme12345678910@gmail.com>
2022-12-13 11:04:01 -0300

    add

--- js/cards.js
+++ js/cards.js
@@ -72,32 +72,6 @@ const listaPizzas =
 
     ]
 
-const listaServicos =
-    [
-        {
-            nome: 'Servicos na loja',
-            foto: '../img/pizza1.png',
-            descricao: 'Os servicos na loja...',
-        },
-        {
-            nome: 'Delivery',
-            foto: '../img/pizza1.png',
-            descricao: 'O delivery é...',
-        },
-        {
-            nome: 'Drive Thru',
-            foto: '../img/pizza1.png',
-            descricao: 'O drive thru...',
-        },
-        {
-            nome: 'Entrega via drone',
-            foto: '../img/pizza1.png',
-            descricao: 'A entrega via drone...',
-        },
-        
-
-    ]
-
 
 const listarTodosProdutos = () => {
 
--- js/cardservicos.js
+++ js/cardservicos.js
@@ -1,3 +1,27 @@
 'use strict'
 
-import { listaServicos } from "./cards";
+const listaServicos =
+    [
+        {
+            nome: 'Servicos na loja',
+            foto: '../img/pizza1.png',
+            descricao: 'Os servicos na loja...',
+        },
+        {
+            nome: 'Delivery',
+            foto: '../img/pizza1.png',
+            descricao: 'O delivery é...',
+        },
+        {
+            nome: 'Drive Thru',
+            foto: '../img/pizza1.png',
+            descricao: 'O drive thru...',
+        },
+        {
+            nome: 'Entrega via drone',
+            foto: '../img/pizza1.png',
+            descricao: 'A entrega via drone...',
+        },
+        
+
+    ]
