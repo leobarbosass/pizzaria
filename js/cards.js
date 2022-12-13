1edc56780739b433cf969b2f4538c9ef4ebbe440
leo <leoextreme12345678910@gmail.com>
2022-12-13 07:34:17 -0300

    add

--- js/cards.js
+++ js/cards.js
@@ -1,14 +1,6 @@
 'use strict'
 
-/* const getAllPizzas = async ()=>{
-    const url = ``
 
-    const response = fetch(url)
-
-    const pizzas = response.json()
-
-    return pizzas
-} */
 
 const listaPizzas =
     [
